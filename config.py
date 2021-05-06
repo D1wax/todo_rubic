913619e2dc79b65c640ec19de3244bc8d52246ee
Andrey Smith <>
2021-05-06 15:55:20 +0000

    Добавили боту команду add

---
+++ config.py
@@ -0,0 +1,10 @@
+#https://api.telegram.org/bot1779928739:AAGH0tYU85ELzVE5ikpodIEFS1lMs7A6w-E/getUpdates
+admin_id = 849333622
+BOT_TOKEN = "1779928739:AAGH0tYU85ELzVE5ikpodIEFS1lMs7A6w-E"
+HELP = """
+/help  - вывод списка команд
+/add  - добавление задачи
+/show  - показать все задачи
+/done  - задача выполнены
+"""
+todo = {}
